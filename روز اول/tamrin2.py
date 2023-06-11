9f06f4c62603c74ef7ae840128d71dde06251609
Mahmoud Fathi <m.fathi1991@gmail.com>
2023-06-11 10:17:21 +0200

    Create tamrin2.py

---
+++ روز اول/tamrin2.py
@@ -0,0 +1,7 @@
+a = input('what is your name: ')
+b = input('how old are you: ')
+c = input('where are you from')
+if a == 'iliya' and  b == '12' and c == 'mashhad':
+    print('GUDRATMAND')
+else:
+    print('no')
