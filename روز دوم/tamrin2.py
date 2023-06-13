017436257fdb1d0596d01f6ae05dc033773f159d
Mahmoud Fathi <m.fathi1991@gmail.com>
2023-06-13 15:33:37 +0200

    Create tamrin2.py

---
+++ روز دوم/tamrin2.py
@@ -0,0 +1,10 @@
+a = input('enter password: ')
+if a =='iliya99' or a == '82358':
+    print('Successful')
+else:
+    print('Password is wrong')
+    b = input('enter pin: ')
+    if b == '1234':
+        print('Successful')
+    else:
+        print('Try again later')
