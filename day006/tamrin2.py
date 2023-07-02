24bdd05b05b5d5862fd269d1b43e1f0c6ac31705
iliyasaf <136832612+iliyasaf@users.noreply.github.com>
2023-07-02 11:21:07 +0330

    Update tamrin2.py

--- day006/tamrin2.py
+++ day006/tamrin2.py
@@ -2,4 +2,4 @@ moz_hay_moz_be_dast = 0
 for a in range(1000):
     if a % 2 == 0:
         moz_hay_moz_be_dast = moz_hay_moz_be_dast + 1
-        print(moz_hay_moz_be_dast)
+print(moz_hay_moz_be_dast)
