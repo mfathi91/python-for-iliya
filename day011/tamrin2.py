9e52a85515d5c79e34c20442dc66335be0601d16
iliyasaf <136832612+iliyasaf@users.noreply.github.com>
2023-07-30 19:47:33 +0330

    Update tamrin2.py

--- day011/tamrin2.py
+++ day011/tamrin2.py
@@ -2,8 +2,6 @@ add = 0
 o = input('enter number : ')
 o = int(o)
 for i in range(1, o):
-        if i % 3 == 0:
-                add = add + 1
-        elif i % 5 == 0:
-                add = add + 1
+    if i % 3 == 0 or i % 5 == 0:
+        add = add + 1
 print(add)
