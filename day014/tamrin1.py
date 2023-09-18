c6b9d21108e47ec3a3b683e1c595d49ae5be37a2
iliyasaf <136832612+iliyasaf@users.noreply.github.com>
2023-09-18 11:22:44 +0330

    Update tamrin1.py

--- day014/tamrin1.py
+++ day014/tamrin1.py
@@ -1,6 +1,8 @@
+bozorgtarin = 0
 file_path = r"C:\files\pycharm\banana.txt"
 with open(file_path, "r") as file:
     for line in file:
         int_line = int(line)
-        if int_line == 5000:
-            print(int_line)
+        if int_line > bozorgtarin:
+            bozorgtarin = int_line
+print(bozorgtarin)
