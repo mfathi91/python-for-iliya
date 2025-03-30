23e5a42075a1b97c0542f7af603fd19491c3b876
iliya90-mod <iliyakingd0m.9@gmail.com>
2025-03-31 00:25:20 +0330

    Update tamrin2.py

--- day016/tamrin2.py
+++ day016/tamrin2.py
@@ -4,5 +4,8 @@ with open(bnana, "r") as file:
     ananb = text.count("a")
     kalm = text.count("p")
     bn = text.count("d")
+    print(f'a = {ananb}')
+    print(f'p = {kalm}')
+    print(f'd = {bn}')
     sus = bnana = ananb + kalm + bn
-    print(sus)
+    print(f'total = {sus}')
